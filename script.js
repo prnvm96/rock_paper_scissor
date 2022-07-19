308145d300565ce79d3fa1b0e05b55bb74958817
prnvm96 <prnvm96@gmail.com>
2022-07-19 19:31:54 +0530

    Modified function(getComputerChoice) in script.js
    The function now return either 'Rock', 'Paper' or 'Scissor' using switch

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3) + 1;
-
+    switch (randomNumber){
+        case 1:
+            console.log("Rock");
+            break;
+        case 2:
+            console.log("Paper");
+            break;
+        case 3:
+            console.log("Scissor");
+            break;
+    }
+    console.log(randomNumber);
 }
 getComputerChoice();
