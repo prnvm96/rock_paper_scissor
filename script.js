af45b69f4e5e42884ec17b5bb3fc072aeb86b8cb
prnvm96 <prnvm96@gmail.com>
2022-07-19 20:54:16 +0530

    Add two functions(singleRound,playerSelection)
    singleRound plays a single round of Rock Paper Scissors and playerSelection for case insensitive input from user

--- script.js
+++ script.js
@@ -11,6 +11,31 @@ function getComputerChoice(){
             console.log("Scissor");
             break;
     }
-    console.log(randomNumber);
 }
-getComputerChoice();
+
+function singleRound(playerSelection,computerSelection){
+    if(playerSelection === computerSelection){
+        return ("It's a Tie!");
+    }
+    else if(playerSelection === "Rock" && computerSelection ==="Paper"){
+        return ("You Lose! Paper beats Rock");
+    }
+    else if(playerSelection === "Paper" && computerSelection ==="Scissor"){
+        return ("You Lose! Scissor beats Paper");
+    }
+    else if(playerSelection === "Scissor" && computerSelection ==="Rock"){
+        return ("You Lose! Rock beats Scissor");
+    }
+    else{
+        return (`You won!  ${playerSelection} beats ${computerSelection}`);
+    }
+
+}
+
+function playerSelection(){
+    const str1 = prompt("Enter your choice:","Rock");
+    const str = str1.charAt(0).toUpperCase() + str1.slice(1).toLowerCase();
+    return str;
+
+}
+console.log(singleRound(playerSelection(),getComputerChoice()));
